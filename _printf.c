5d24b5c9fb05d1bca9357e24d10fe39e4376254f
fortune-07 <fortuneniguel@gmail.com>
2021-03-17 08:42:44 +0000

    fixed error

--- _printf.c
+++ _printf.c
@@ -1,45 +1,30 @@
 #include "holberton.h"
 /**
-* _printf - produces output according to a format
-* @format: format string containing the characters and the specifiers
-* Description: this function will call the get_print() function that will
-* determine which printing function to call depending on the conversion
-* specifiers contained into fmt
-* Return: length of the formatted output string
-*/
+* _printf - print string as printf
+* @format: number of arguments
+* Return: void
+**/
 int _printf(const char *format, ...)
 {
-int (*pfunc)(va_list, flags_t *);
-const char *p;
-va_list arguments;
-flags_t flags = {0, 0, 0};
-register int count = 0;
-va_start(arguments, format);
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
-for (p = format; *p; p++)
+va_list ap;
+unsigned int i, result;
+
+result = 0;
+va_start(ap, format);
+
+for (i = 0; format[i] != '\0'; i++)
 {
-if (*p == '%')
+if (format[i] == '%')
 {
-p++;
-if (*p == '%')
-{
-count += _putchar('%');
-continue;
-}
-while (get_flag(*p, &flags))
-p++;
-pfunc = get_print(*p);
-count += (pfunc)
-? pfunc(arguments, &flags)
-: _printf("%%%c", *p);
+result += pull_print(format[i + 1], &ap);
+i++;
 }
 else
-count += _putchar(*p);
+{
+_putchar(format[i]);
+result++;
 }
-_putchar(-1);
-va_end(arguments);
-return (count);
+}
+va_end(ap);
+return (result);
 }
--- holberton.h
+++ holberton.h
@@ -2,63 +2,27 @@
 #define HOLBERTON_H
 #include <stdlib.h>
 #include <stdarg.h>
-/**
-* struct flags - struct containing flags to "turn on"
-* when a flag specifier is passed to _printf()
-* @plus: flag for the '+' character
-* @space: flag for the ' ' character
-* @hash: flag for the '#' character
-*/
-typedef struct flags
-{
-	int plus;
-	int space;
-	int hash;
-} flags_t;
-/**
-* struct printHandler - struct to choose the right function depending
-* on the format specifier passed to _printf()
-* @c: format specifier
-* @f: pointer to the correct printing function
-*/
-typedef struct printHandler
-{
-	char c;
-	int (*f)(va_list ap, flags_t *f);
-} ph;
-/* print_nums */
-int print_int(va_list l, flags_t *f);
-void print_number(int n);
-int print_unsigned(va_list l, flags_t *f);
-int count_digit(int i);
-/* print_bases */
-int print_hex(va_list l, flags_t *f);
-int print_hex_big(va_list l, flags_t *f);
-int print_binary(va_list l, flags_t *f);
-int print_octal(va_list l, flags_t *f);
-/* converter */
-char *convert(unsigned long int num, int base, int lowercase);
-/* _printf */
+
+int _putchar(char);
+int pull_print(char c, va_list *ap);
 int _printf(const char *format, ...);
-/* get_print */
-int (*get_print(char s))(va_list, flags_t *);
-/* get_flag */
-int get_flag(char s, flags_t *f);
-/* print_alpha */
-int print_string(va_list l, flags_t *f);
-int print_char(va_list l, flags_t *f);
-/* write_funcs */
-int _putchar(char c);
-int _puts(char *str);
-/* print_custom */
-int print_rot13(va_list l, flags_t *f);
-int print_rev(va_list l, flags_t *f);
-int print_bigS(va_list l, flags_t *f);
+int exponent(int x, int y);
+unsigned long binary(unsigned int num);
+int p_int(va_list *);
+int p_char(va_list *);
+int p_float(va_list *);
+int p_string(va_list *);
+int p_rev(char *s);
 
-/* print_address */
-int print_address(va_list l, flags_t *f);
-
-/* print_percent */
-int print_percent(va_list l, flags_t *f);
-
+/**
+* struct flag -flag object
+* @letter: flag char
+* @prnt: print function pointer
+* Descriptions: contains flag letter and corresponding print function
+**/
+typedef struct flag
+{
+char letter;
+int (*prnt)(va_list *);
+} find_flag;
 #endif
