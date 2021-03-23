b5acc89e6898d47b908097623cda673bdb2b269e
fortune-07 <fortuneniguel@gmail.com>
2021-03-23 17:59:47 +0000

    printf

--- _printf.c
+++ _printf.c
@@ -1,30 +1,52 @@
 #include "holberton.h"
 /**
-* _printf - print string as printf
-* @format: number of arguments
-* Return: void
-**/
+* _printf - function that prints based on format specifier
+* @format: takes in format specifier
+* Return: return pointer to index
+*/
 int _printf(const char *format, ...)
 {
-va_list ap;
-unsigned int i, result;
-
-result = 0;
-va_start(ap, format);
-
+char buffer[1024];
+int i, j = 0, a = 0, *index = &a;
+va_list valist;
+vtype_t spec[] = {
+{'c', format_c}, {'d', format_d}, {'s', format_s}, {'i', format_d},
+{'u', format_u}, {'%', format_perc}, {'x', format_h}, {'X', format_ch},
+{'o', format_o}, {'b', format_b}, {'p', format_p}, {'r', format_r},
+{'R', format_R}, {'\0', NULL}
+};
+if (!format)
+return (-1);
+va_start(valist, format);
 for (i = 0; format[i] != '\0'; i++)
 {
+for (; format[i] != '%' && format[i] != '\0'; *index += 1, i++)
+{
+if (*index == 1024)
+{
+_write_buffer(buffer, index);
+reset_buffer(buffer);
+*index = 0;
+}
+buffer[*index] = format[i];
+}
+if (format[i] == '\0')
+break;
 if (format[i] == '%')
 {
-result += pull_print(format[i + 1], &ap);
 i++;
-}
-else
+for (j = 0; spec[j].tp != '\0'; j++)
 {
-_putchar(format[i]);
-result++;
+if (format[i] == spec[j].tp)
+{
+spec[j].f(valist, buffer, index);
+break;
 }
 }
-va_end(ap);
-return (result);
+}
+}
+va_end(valist);
+buffer[*index] = '\0';
+_write_buffer(buffer, index);
+return (*index);
 }
--- holberton.h
+++ holberton.h
@@ -1,28 +1,47 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdlib.h>
 #include <stdarg.h>
-
-int _putchar(char);
-int pull_print(char c, va_list *ap);
-int _printf(const char *format, ...);
-int exponent(int x, int y);
-unsigned long binary(unsigned int num);
-int p_int(va_list *);
-int p_char(va_list *);
-int p_float(va_list *);
-int p_string(va_list *);
-int p_rev(char *s);
-
+#include <stdio.h>
 /**
-* struct flag -flag object
-* @letter: flag char
-* @prnt: print function pointer
-* Descriptions: contains flag letter and corresponding print function
-**/
-typedef struct flag
+* struct vtype - struct vtype
+* @tp: tp
+* @f: function
+*
+*/
+typedef struct vtype
 {
-char letter;
-int (*prnt)(va_list *);
-} find_flag;
-#endif
+	char tp;
+	void (*f)();
+} vtype_t;
+int _printf(const char *format, ...);
+void print_char(va_list valist);
+void print_int(va_list valist);
+void print_float(va_list valist);
+void print_string(va_list valist);
+void _write_buffer(char *buffer, int *index);
+int _strlen(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void format_s(va_list valist, char *buffer, int *index);
+void format_c(va_list valist, char *buffer, int *index);
+void format_d(va_list valist, char *buffer, int *index);
+char *itos(char str[], long int num);
+char *utos(char str[], int num);
+int num_len(int num);
+int float_len(double f);
+void format_i(va_list valist, char *buffer, int *index);
+void format_u(va_list valist, char *buffer, int *index);
+void format_perc(va_list valist, char *buffer, int *index);
+void format_p(va_list valist, char *buffer, int *index);
+void format_lx(va_list valist, char *buffer, int *index);
+char *tostring(char str[], int num);
+int num_len(int num);
+void reset_buffer(char buffer[]);
+void *rot13(char *s);
+void rev_string(char *s);
+void format_h(va_list valist, char *buffer, int *index);
+void format_ch(va_list valist, char *buffer, int *index);
+void format_o(va_list valist, char *buffer, int *index);
+void format_b(va_list valist, char *buffer, int *index);
+void format_r(va_list valist, char *buffer, int *index);
+void format_R(va_list valist, char *buffer, int *index);
+#endif /* HOLBERTON_H */
